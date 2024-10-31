a00924e1665f3dbe875f5cb26d192a2c5640e723
HollySloat <157514488+DollyHalo@users.noreply.github.com>
2024-10-31 08:37:50 -0500

    add handprint, changed stars to blood

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ const element = docuent.getElementById('enter-button')
 const intro= document.getElementById('intro'):
 element.addeventlistener('click', () => {
 });
+
+
+// JS scripts placed here
+const element = document.getElementById('enter-button');
+const intro = document.getElementById('intro');
+
+element.addEventListener('click', () => {
+    intro.classList.add('fade-out');
+});
